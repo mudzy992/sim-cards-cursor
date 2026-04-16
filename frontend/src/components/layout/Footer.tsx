33630401d961c5c13847e97abf654f7be752c11b
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2026-04-16 14:11:44 +0200

    Verzionisanje

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { Layout, Typography } from 'antd';
+import { appVersion, formattedAppBuildDate } from '@/config/app-version';
 
 const { Footer: AntFooter } = Layout;
 
@@ -6,7 +7,7 @@ export function Footer() {
   return (
     <AntFooter className="!bg-transparent !text-center">
       <Typography.Text type="secondary">
-        SIM Tracker - V1 - 12.03.2026.
+        {`SIM Tracker - v${appVersion} - ${formattedAppBuildDate}.`}
       </Typography.Text>
     </AntFooter>
   );
