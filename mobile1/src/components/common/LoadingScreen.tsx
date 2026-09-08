1ddb9f478d20b27efeb0ccd790ab14072c014cd2
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2026-09-08 13:03:10 +0200

    Brisanje bacupa

--- mobile1/src/components/common/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator, View } from 'react-native';
-
-export function LoadingScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <ActivityIndicator size="large" />
-    </View>
-  );
-}
--- mobile1/src/types/api.types.ts
+++
@@ -1,4 +0,0 @@
-export type ApiEnvelope<T> = {
-  success: boolean;
-  data: T;
-};
